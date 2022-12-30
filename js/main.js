694d27b6e134eeb14711883bd6468c222c179589
Dan <DanRHatfield@gmail.com>
2022-12-30 10:00:52 -0700

    Bringing repo up to date

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("services").addEventListener('click', displayservices)
+
+function displayservices () {
+    document.getElementById("services_list").style.display = 'block'
+}
