d76fe8687dad6a71de24309f8991163102fea3fc
Dan <DanRHatfield@gmail.com>
2023-05-12 15:03:43 -0600

    Changed how images in the services section work

--- js/internal.js
+++ js/internal.js
@@ -3,10 +3,33 @@ let selectedImage = document.querySelectorAll(".ser_img")
 let vpHeight = window.innerHeight
 let vpWidth = window.innerWidth
 
-document.getElementById('vpResult').innerText = vpHeight + ' ' + vpWidth
-selectedImage.forEach(el => {
-    el.addEventListener('click', showFullImage)
-})
+let numImages = document.querySelectorAll(".serviceContent").length
+let servCont = document.querySelectorAll(".serviceContent")
+let servImg = document.querySelectorAll(".ser_imgSharpening")
+
+if(numImages > 2) {
+    console.log("True")
+    servCont.forEach(el => {
+        el.classList.toggle("threeColumn_content")
+    })
+    servImg.forEach(el => (
+        el.classList.toggle("threeColumn_img")
+    ))
+}
+else if(numImages == 2) {
+    console.log("2")
+    servCont.forEach(el => {
+        el.classList.toggle("twoColumn_content")
+    })
+    servImg.forEach(el => (
+        el.classList.toggle("twoColumn_img")
+    ))
+}
+
+// document.getElementById('vpResult').innerText = vpHeight + ' ' + vpWidth
+// selectedImage.forEach(el => {
+//     el.addEventListener('click', showFullImage)
+// })
 
 function goHome() {
     open("index.html", '_self')
